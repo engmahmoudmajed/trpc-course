c61b7b1905cef3c802d3cc96d998b2fc2d72cdf9
Eng Mahmoud Majed <mahmoudmajed404@gmail.com>
2026-07-05 20:20:08 +0300

    0 : Ready To understand tRPC

---
+++ README.md
@@ -0,0 +1,4 @@
+# tRPC Course
+
+**This Readme File Will Explaint To you trpc**
+
---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>
+        welcome to TRPC
+      </h1>
+    </>
+  )
+}
+
+export default App
